f78088366d8449efc068a7d526418760e9df3d06
skyrichardson <86213662+skyrichardson@users.noreply.github.com>
2025-08-29 14:23:38 +1000

    Add note about account not being stored  in .env
    DUxxxxxxx account are not sensitive information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-account = 'DU3062142'
+account = 'DU3062142'  # Dummy trading account so does not need to be private
 time_in_force = 'DAY'
 risk_per_trade_dollars = 100
 basket_tag = 'SET_DAY_TRADES'
@@ -14,7 +14,7 @@ def main():
     orders = [
         [
             'Action', 'Quantity', 'Symbol', 'Exchange', 'Currency', 'TimeInForce', 'OrderType',
-            'LmtPrice', 'OcaGroup', 'OrderId', 'BasketTag', 'Account']
+            'LmtPrice', 'OcaGroup', 'OrderId', 'BasketTag']  # 'Account'
     ]
     for position, sublist in enumerate(list_of_lists[1:]):
         limit_price = float(sublist[3])
@@ -32,12 +32,12 @@ def main():
             stop_action = 'BUY'
         order = [
             action, int(round(quantity, 0)), sublist[2], 'SMART', 'USD', 'DAY', 'LMT',
-            limit_price, '', position, '', basket_tag, account]
+            limit_price, '', position, '', basket_tag]  #, account]
         # print(order)
         orders.append(order)
         order = [
             stop_action, int(round(quantity, 0)), sublist[2], 'SMART', 'USD', 'DAY', 'LMT',
-            aux_price, position, '', position, basket_tag, account]
+            aux_price, position, '', position, basket_tag]  # , account]
         orders.append(order)
     # print(orders)
     with open('orders.csv', 'w', newline='') as file:
